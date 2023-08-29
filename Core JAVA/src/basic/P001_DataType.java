3d2a02e0d8ad8399a71b40799527755798f64212
jay-java <jayprajapati.tops@gmail.com>
2023-08-29 19:11:37 +0530

    this is first commit in selenium

---
+++ Core JAVA/src/basic/P001_DataType.java
@@ -0,0 +1,14 @@
+package basic;
+//datatype -> type of data user wants to access for operation
+//Two types 
+//1.Primitive -> byte,short,int,long,float,double,boolean,char
+//					byte,short,int,long -> whole number
+//					float,double -> decimles
+//					boolean -> true/false
+//					char -> character
+//2.Non-Primitive -> Array,String,Class
+public class P001_DataType {
+	public static void main(String[] args) {
+		short b = 128;
+	}
+}
