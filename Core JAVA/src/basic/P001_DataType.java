a67e3c7cd93899b6d2601d850072b9fe0a62a656
jay-java <jayprajapati.tops@gmail.com>
2023-08-29 19:41:05 +0530

    datatypes

--- Core JAVA/src/basic/P001_DataType.java
+++ Core JAVA/src/basic/P001_DataType.java
@@ -1,4 +1,5 @@
 package basic;
+
 //datatype -> type of data user wants to access for operation
 //Two types 
 //1.Primitive -> byte,short,int,long,float,double,boolean,char
@@ -9,6 +10,23 @@ package basic;
 //2.Non-Primitive -> Array,String,Class
 public class P001_DataType {
 	public static void main(String[] args) {
-		short b = 128;
+		byte b = 127;
+		System.out.println(b);
+		short s = 128;
+		System.out.println(s);
+		int i = 1235;
+		long l = 34656765765767676l;
+		float f = 3.1456869679f;
+		System.out.println(f);
+		double d = 3.1456869679;
+		System.out.println(d);
+		boolean b1 = false;
+		char c = 'a';
+		System.out.println(c);
+		double a = 3.0+2.0;
+		System.out.println(a);
+		double base = 5.0,height=6.5;
+		double area = (base*height)/2;
+		System.out.println(area);
 	}
 }
