5c89c377942c1752f51f16f41cc06726458e8a65
jay-java <jayprajapati.tops@gmail.com>
2023-09-14 19:38:48 +0530

    static this

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/5d/b0052e340453001e1b71cd1a188e13b9
@@ -0,0 +1,7 @@
+package basic;
+
+public class P013_Inheritance {
+	public static void main(String[] args) {
+		
+	}
+}
--- Core JAVA/src/basic/P008_Array.java
+++ Core JAVA/src/basic/P008_Array.java
@@ -6,6 +6,42 @@ import java.util.Scanner;
 //types -> 1.single dimensional 2.multi
 public class P008_Array {
 	public static void main(String[] args) {
+		Scanner sc=  new Scanner(System.in);
+		//Multi dimensional array
+		int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+		for(int row=0;row<=2;row++) {
+			for(int col=0;col<=2;col++) {
+				System.out.print(arr[row][col]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println("enter num of rows : ");
+		int r = sc.nextInt();
+		System.out.println("enter num of cols : ");
+		int c = sc.nextInt();
+		int arr1[][] = new int[r][c];
+		for(int row=0;row<r;row++) {
+			for(int col=0;col<c;col++) {
+				System.out.print("enter ele at arr1["+row+"]["+col+"] : ");
+				arr1[row][col] = sc.nextInt();
+			}
+		}
+		System.out.println("final array : ----->");
+		for(int row=0;row<r;row++) {
+			for(int col=0;col<c;col++) {
+				System.out.print(arr1[row][col]+" ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		//single dimensional array
+		
 		int i=1,j=2,k=3,l=4,p=5,o=6;
 		System.out.println(j);
 		int b[] = new int[20];
@@ -15,7 +51,7 @@ public class P008_Array {
 		for(int index=0;index<=9;index++) {
 			System.out.println(a[index]);
 		}
-		Scanner sc=  new Scanner(System.in);
+		
 		System.out.println("enter size of array : ");
 		int size = sc.nextInt();
 		int ar[] = new int[size];
